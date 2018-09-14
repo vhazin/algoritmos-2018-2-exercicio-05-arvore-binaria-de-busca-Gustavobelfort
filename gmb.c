03290d569cad7b6a30e177a5a70d535ad28c9ae4
Gustavo Belfort <gustavobelfort@outlook.com>
2018-09-14 15:33:21 -0300

    Create gmb.c

---
+++ gmb.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *root = NULL;
+
+int insert(int data, struct node **leaf){
+  if(*leaf == 0){
+      *leaf = (struct node*) malloc(sizeof(struct node));
+      (*leaf)->data = data;
+      (*leaf)->left = NULL;
+      (*leaf)->right = NULL;
+  } else if( data < (*leaf)->data ){
+      insert( data, &(*leaf)->left );
+  } else if( data > (*leaf)->data ){
+      insert( data, &(*leaf)->right );
+  }  
+
+    return 0;
+}
+
+void destroy(struct node *leaf){
+    if( leaf != NULL ){
+        destroy(leaf->left);
+        destroy(leaf->right);
+        free(leaf);
+    }
+}
+
+void postorder(struct node* node){
+
+    if(node == NULL){
+        return;
+    }
+
+    postorder(node->left);
+    postorder(node->right);
+    printf(" %d", node->data);
+}
+
+void inorder(struct node* node){
+
+    if(node == NULL){
+        return;
+    }
+
+    inorder(node->left);
+    printf(" %d", node->data);
+    inorder(node->right);
+}
+
+void preorder(struct node* node){
+
+    if(node == NULL){
+        return;
+    }
+
+    printf(" %d", node->data);
+    preorder(node->left);
+    preorder(node->right);
+
+}
+
+int main(){
+    
+    int testNumber, nodesNumber, element;
+
+    scanf("%d", testNumber);
+    for(int i; i < testNumber; i++){
+    scanf("%d", &nodesNumber);
+
+    for(int i = 0; i < nodesNumber; i++){
+        scanf("%d", &element);
+        insert(element, &root);
+    }
+
+    printf("Case %d:",i);
+    preorder(root);
+    postorder(root);
+
+    testNumber--;
+    }
+}
